a917cc5113076527035507a04a7e0f498af2e478
Daniel Ziomek <danielziomek150@gmail.com>
2022-01-26 00:11:52 +0100

    Dynamically creating labels and entries

--- app.py
+++ app.py
@@ -9,89 +9,59 @@ class AmountError(Exception):
 class Converter(object):
     def __init__(self):
         self.window = Tk()
-        self.window.geometry("800x600")
+        self.window.geometry("250x350")
+        self.window.eval("tk::PlaceWindow . center")
         self.window.title("KONWERTER Waluty PLN ")
         self.build()
         self.window.mainloop()
 
     def build(self):
-        self.field1 = Label()
-        self.field1["text"] = "Podaj kwote w zl:"
-        self.field1.grid(row=0, column=0, sticky="w")
+        # PLN currency label and entry field
+        self.frame_pln = Frame()
+        self.label_pln = Label(
+            self.frame_pln, width=15, text="Podaj kwote w PLN:", anchor="w"
+        )
+        self.entry_pln = Entry(self.frame_pln)
+        self.frame_pln.pack(side=TOP, fill=X, padx=5, pady=5)
+        self.label_pln.pack(side=LEFT)
+        self.entry_pln.pack(side=LEFT)
 
-        self.description1 = Entry(width=10)
-        self.description1.focus()
-        self.description1.grid(row=0, column=1, sticky="w")
+        self.entry_pln.focus()
 
-        self.button1 = Button(
+        self.frame_btn = Frame()
+        self.button = Button(
+            self.frame_btn,
             activebackground="grey",
             activeforeground="white",
             relief="groove",
-            width=20,
+            height=2,
+            width=25,
             text="PRZELICZ",
             command=self.convert,
         )
-        self.button1.grid(columnspan=2, row=1, column=0, padx=10, pady=10, sticky="w")
+        self.frame_btn.pack(side=TOP, fill=X, padx=30, pady=5)
+        self.button.pack(side=LEFT)
 
-        self.field2 = Label()
-        self.field2["text"] = "Oto Kwota w USD:"
-        self.field2.grid(row=2, column=0, sticky="w")
-
-        self.description2 = Entry(width=10)
-        self.description2.grid(row=2, column=1, sticky="w")
-
-        self.field3 = Label()
-        self.field3["text"] = "Oto Kwota w EUR:"
-        self.field3.grid(row=4, column=0, sticky="w")
-
-        self.description3 = Entry(width=10)
-        self.description3.grid(row=4, column=1, sticky="w")
-
-        self.field4 = Label()
-        self.field4["text"] = "Oto Kwota w JPY:"
-        self.field4.grid(row=6, column=0, sticky="w")
-
-        self.description4 = Entry(width=10)
-        self.description4.grid(row=6, column=1, sticky="w")
-
-        self.field5 = Label()
-        self.field5["text"] = "Oto Kwota w GBP:"
-        self.field5.grid(row=8, column=0, sticky="w")
-
-        self.description5 = Entry(width=10)
-        self.description5.grid(row=8, column=1, sticky="w")
-
-        self.field6 = Label()
-        self.field6["text"] = "Oto Kwota w AUD:"
-        self.field6.grid(row=10, column=0, sticky="w")
-
-        self.description6 = Entry(width=10)
-        self.description6.grid(row=10, column=1, sticky="w")
-
-        self.field7 = Label()
-        self.field7["text"] = "Oto Kwota w CAD:"
-        self.field7.grid(row=12, column=0, sticky="w")
-
-        self.description7 = Entry(width=10)
-        self.description7.grid(row=12, column=1, sticky="w")
-
-        self.field8 = Label()
-        self.field8["text"] = "Oto Kwota w CHF:"
-        self.field8.grid(row=14, column=0, sticky="w")
-
-        self.description8 = Entry(width=10)
-        self.description8.grid(row=14, column=1, sticky="w")
-
-        self.field9 = Label()
-        self.field9["text"] = "Oto Kwota w SEK:"
-        self.field9.grid(row=16, column=0, sticky="w")
-
-        self.description9 = Entry(width=10)
-        self.description9.grid(row=16, column=1, sticky="w")
+        string = "Oto kwota w "
+        currencies = ["USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "SEK"]
+        self.labels = []  # creates an empty list for labels
+        self.entries = []  # creates an empty list for entries
+        for x in currencies:  # iterates over currencies
+            label_name = string + x + ":"
+            row = Frame()
+            label = Label(row, width=15, text=label_name, anchor="w")
+            entry = Entry(row, justify="right")
+            row.pack(side=TOP, fill=X, padx=5, pady=5)
+            label.pack(side=LEFT)
+            entry.pack(side=LEFT)
+            self.labels.append(label)  # appends the label to the list for further use
+            self.entries.append(
+                entry
+            )  # appends the entries to the list for further use
 
     def convert(self):
         try:
-            t = float(self.description1.get())
+            t = float(self.entry_pln.get())
             if t <= 0:
                 raise AmountError
         except ValueError:
@@ -99,17 +69,6 @@ class Converter(object):
         except AmountError:
             messagebox.showerror("AmountError", "Kwota nieprawidłowa")
         else:
-            descriptions = [
-                self.description2,
-                self.description3,
-                self.description4,
-                self.description5,
-                self.description6,
-                self.description7,
-                self.description8,
-                self.description9,
-            ]
-
             currency_rates = [
                 t / 4.06,
                 t / 4.59,
@@ -121,9 +80,9 @@ class Converter(object):
                 t / 0.44,
             ]
 
-            for i in range(len(descriptions)):
-                descriptions[i].delete(0, END)
-                descriptions[i].insert(0, str(round(currency_rates[i], 2)))
+            for i in range(len(self.entries)):
+                self.entries[i].delete(0, END)
+                self.entries[i].insert(0, str(round(currency_rates[i], 2)))
 
 
-k = Converter()
+converter = Converter()
